a0827adbfa4cdef11864294725c05935e592f664
Rodri10a <rodrigojosearguello@gmail.com>
2026-03-14 15:18:26 -0300

    feat(player): metodo move con lectura de InputHandler

--- tereré-game/entities/player.py
+++ tereré-game/entities/player.py
@@ -19,3 +19,37 @@ class Player(Character):
         self.is_attacking: bool = False
         self.is_blocking: bool = False
         self.attack_active_frames: int = 0
+
+    def handle_input(self) -> None:
+        """Procesa el input del jugador."""
+        if self.hurt_timer > 0:
+            return
+
+        # Movimiento
+        if self.input_handler.is_pressed("MOVE_LEFT"):
+            self.vel_x = -self.speed
+            self.direction = -1
+            if self.state not in ("attacking", "jumping"):
+                self.state = "running"
+        elif self.input_handler.is_pressed("MOVE_RIGHT"):
+            self.vel_x = self.speed
+            self.direction = 1
+            if self.state not in ("attacking", "jumping"):
+                self.state = "running"
+        else:
+            if self.state == "running":
+                self.state = "idle"
+
+        # Saltar
+        if self.input_handler.was_just_pressed("JUMP") and self.on_ground:
+            self.vel_y = JUMP_FORCE
+            self.on_ground = False
+            self.state = "jumping"
+
+        # Ataque normal (combo de 2 golpes)
+        if self.input_handler.was_just_pressed("ATTACK") and self.attack_cooldown <= 0:
+            self.attack()
+
+        # Ataque especial
+        if self.input_handler.was_just_pressed("SPECIAL") and self.special_cooldown <= 0:
+            self.special_attack()
