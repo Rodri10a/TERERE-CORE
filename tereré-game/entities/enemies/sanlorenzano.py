7c32bc7770dd712634804656e7ad5c323dff2bfb
rodri10a <rodrigojosearguello@gmail.com>
2026-03-18 08:02:08 -0300

    chore: eliminar codigo muerto y archivos sin uso
    - Eliminar subclases de enemigos no utilizadas (capiateno, cheto, guardia, luqueno, sanlorenzano)
    - Eliminar projectile.py y physics.py sin referencias en el proyecto
    - Eliminar assets sin uso (.gitkeep, archivos de musica)

--- tereré-game/entities/enemies/sanlorenzano.py
+++
@@ -1,9 +0,0 @@
-from entities.enemy import Enemy
-
-class Salorensano(Enemy):
-    #Clase especifica del enemigo de Luque
-    def __init__(self, x, y):
-        super().__init__(x, y, speed=3.0, health=100, damage=10)
-
-        self.chase_range = 300
-        self.attack_range = 80
