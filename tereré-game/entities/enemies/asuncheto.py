d60928007da2866eeb44fb075d5ad19faf708d24
rodri10a <rodrigojosearguello@gmail.com>
2026-03-18 08:07:44 -0300

    chore: eliminar clase asuncheto no utilizada

--- tereré-game/entities/enemies/asuncheto.py
+++
@@ -1,9 +0,0 @@
-from entities.enemy import Enemy
-
-class Asuncheto(Enemy):
-    #Clase especifica del enemigo de Asuncion
-    def __init__(self, x, y):
-        super().__init__(x, y, speed=4.0, health=200, damage=20)
-
-        self.chase_range = 350
-        self.attack_range = 85
